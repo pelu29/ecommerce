b4dfb5ef12588fc3f5ab0142963908af79b3059e
pelu29 <floresjack74@gmail.com>
2025-12-30 15:29:18 -0500

    Agregando sección inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'inicio', component:Inicio},
+];
